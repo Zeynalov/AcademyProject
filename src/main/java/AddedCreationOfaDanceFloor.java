186adcfc7786103375673f04720a050643ceda8e
AlexeyZeynalov <6361773@mail.ru>
2018-05-03 11:07:11 +0300

    added method dancer mover

--- src/main/java/AddedCreationOfaDanceFloor.java
+++ src/main/java/AddedCreationOfaDanceFloor.java
@@ -18,4 +18,26 @@ public class AddedCreationOfaDanceFloor {
             }
         }
     }
+
+    public void dancerMove() {
+        int position = 0, countStep = 0, count = 0;
+        addedSequenceCheck.myMap.put(position, count);
+        try {
+            while (true) {
+                if (addedZeroCheckAndOutOfBounds.checkInZeroAndUneven(arrayList)) {
+                    System.out.println("Вы ввели 0 или нечетное число, введите четное число, чтобы танцор мог двигаться");
+                    break;
+                }
+                if (arrayList.get(position) % 2 == 0) {
+                    position += arrayList.get(position);
+                } else {
+                    position -= arrayList.get(position);
+                }
+                countStep = addedZeroCheckAndOutOfBounds.checkIndexOutOfBounds(arrayList, countStep, position);
+                position = addedSequenceCheck.loopBackCheck(position);
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Танцор сделал " + countStep + " шага(ов)");
+        }
+    }
 }
