cbc4ff32f956118991e147f4b8c7c4226f21bca3
AlexeyZeynalov <6361773@mail.ru>
2018-05-03 11:20:04 +0300

    added method check index out of bounds

--- src/main/java/AddedZeroCheckAndOutOfBounds.java
+++ src/main/java/AddedZeroCheckAndOutOfBounds.java
@@ -5,4 +5,14 @@ public class AddedZeroCheckAndOutOfBounds {
     public boolean checkInZeroAndUneven(ArrayList<Integer> arrayList) {
         return (arrayList.get(0).equals(0) || arrayList.get(0) % 2 != 0);
     }
+
+    public int checkIndexOutOfBounds(ArrayList<Integer> arrayList, int countStep, int position) {
+        if (countStep + arrayList.get(position) > arrayList.size() - 1) {
+            int temp = arrayList.get(position) - arrayList.size() - 1;
+            countStep += arrayList.get(position) - temp;
+        } else {
+            countStep += arrayList.get(position);
+        }
+        return countStep;
+    }
 }
