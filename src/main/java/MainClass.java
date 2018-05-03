4ba201ab2b4a6b4f4e2220a1acd570e56d0f6eda
AlexeyZeynalov <6361773@mail.ru>
2018-05-03 11:26:05 +0300

    added instance of a class

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,9 @@
 public class MainClass {
     public static void main(String[] args) {
-        
+
+        AddedCreationOfaDanceFloor addedCreationOfaDanceFloor = new AddedCreationOfaDanceFloor();
+
+        addedCreationOfaDanceFloor.createPlatform(2, 0, 7);
+        addedCreationOfaDanceFloor.dancerMove();
     }
 }
